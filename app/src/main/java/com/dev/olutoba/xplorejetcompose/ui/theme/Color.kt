74ea3cd5f0dd6fd319f176c95eca0dffa110d3f2
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-07-07 13:52:18 +0100

    modify Theme.kt and Color.kt files

--- app/src/main/java/com/dev/olutoba/xplorejetcompose/ui/theme/Color.kt
+++ app/src/main/java/com/dev/olutoba/xplorejetcompose/ui/theme/Color.kt
@@ -9,3 +9,13 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val onPrimary = Color(0xFFFFFFFF)
+val onSecondary = Color(0xFFFFFFFF)
+val onTertiary = Color(0xFFFFFFFF)
+
+val background = Color(0xFFFFFBFE)
+val onBackground = Color(0xFF1C1B1F)
+
+val surface = Color(0xFFFFFBFE)
+val onSurface = Color(0xFF1C1B1F)
