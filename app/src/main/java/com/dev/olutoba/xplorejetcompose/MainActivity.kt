62e128810b601195dcfec56d7562f165c08ff0da
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-07-30 01:23:58 +0100

    implement business card app UI

--- app/src/main/java/com/dev/olutoba/xplorejetcompose/MainActivity.kt
+++ app/src/main/java/com/dev/olutoba/xplorejetcompose/MainActivity.kt
@@ -12,6 +12,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -28,6 +31,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.dev.olutoba.xplorejetcompose.ui.theme.ColumnTitleColor
 import com.dev.olutoba.xplorejetcompose.ui.theme.ImageTitleColor
+import com.dev.olutoba.xplorejetcompose.ui.theme.LightGreenBackgroundColor
 import com.dev.olutoba.xplorejetcompose.ui.theme.RowTitleColor
 import com.dev.olutoba.xplorejetcompose.ui.theme.TextTitleColor
 import com.dev.olutoba.xplorejetcompose.ui.theme.XploreJetComposeTheme
@@ -239,6 +243,89 @@ private fun QuadrantInfoCard(
 }
 
 
+@Composable
+private fun BusinessCardApp() {
+    Column(
+        modifier = Modifier.background(LightGreenBackgroundColor)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.android_logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .background(Color.Black)
+                    .size(100.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.jennifer_doe),
+                fontSize = 50.sp
+            )
+
+            Text(text = stringResource(R.string.android_developer_experience))
+        }
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(0.2f)
+        ) {
+            ContactInfoData(
+                title = stringResource(R.string.text_phone_number),
+                icon = painterResource(R.drawable.baseline_call_24)
+            )
+
+            ContactInfoData(
+                title = stringResource(R.string.text_share_with),
+                icon = painterResource(R.drawable.baseline_share_24)
+            )
+
+            ContactInfoData(
+                title = stringResource(R.string.text_email),
+                icon = painterResource(R.drawable.baseline_email_24)
+            )
+        }
+    }
+}
+
+@Composable
+fun ContactInfoData(
+    title: String,
+    icon: Painter,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(4.dp)
+    ) {
+        Icon(
+            painter = icon,
+            contentDescription = null,
+            modifier = modifier
+                .weight(0.4f)
+        )
+
+        Text(
+            text = title,
+            textAlign = TextAlign.Start,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = modifier
+                .weight(0.7f)
+        )
+    }
+}
+
+
 @Preview(
     showBackground = true,
     showSystemUi = true,
@@ -246,6 +333,6 @@ private fun QuadrantInfoCard(
 @Composable
 fun GreetingPreview() {
     XploreJetComposeTheme {
-        ComposeQuadrantApp()
+        BusinessCardApp()
     }
 }
