7ad3fbc1bed74a1f07bab664943ce350d28b42c4
Damola Olutoba Onikoyi <damexxey94@gmail.com>
2025-08-10 13:11:14 +0100

    move components into ui package

--- app/src/main/java/com/dev/olutoba/xplorejetcompose/ui/components/ArticleCard.kt
+++ app/src/main/java/com/dev/olutoba/xplorejetcompose/ui/components/ArticleCard.kt
@@ -1,4 +1,4 @@
-package com.dev.olutoba.xplorejetcompose.components
+package com.dev.olutoba.xplorejetcompose.ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
