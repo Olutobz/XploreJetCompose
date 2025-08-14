15e1fe2e49bf0f50151198425d8f7bee51acacfd
Damola Olutoba Onikoyi <damexxey94@gmail.com>
2025-08-14 23:15:05 +0100

    create feature app to calculate tip percentage

---
+++ app/src/main/java/com/dev/olutoba/xplorejetcompose/ui/components/TipTimeApp.kt
@@ -0,0 +1,16 @@
+package com.dev.olutoba.xplorejetcompose.ui.components
+
+import androidx.compose.runtime.Composable
+
+
+/**
+ * Created by Onikoyi Damola Olutoba
+ * DATE: 14 August, 2025
+ * EMAIL: damexxey94@gmail.com
+ */
+
+
+@Composable
+fun TipTimeLayout() {
+
+}
