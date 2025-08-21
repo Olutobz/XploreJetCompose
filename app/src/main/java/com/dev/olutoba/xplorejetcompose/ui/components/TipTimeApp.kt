8296838106e285d0b6e13d9237c9de030c816f07
Damola Olutoba Onikoyi <damexxey94@gmail.com>
2025-08-21 17:08:30 +0100

    setup starter code for Tip Calculator Layout

--- app/src/main/java/com/dev/olutoba/xplorejetcompose/ui/components/TipTimeApp.kt
+++ app/src/main/java/com/dev/olutoba/xplorejetcompose/ui/components/TipTimeApp.kt
@@ -1,5 +1,18 @@
 package com.dev.olutoba.xplorejetcompose.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dev.olutoba.xplorejetcompose.ui.theme.XploreJetComposeTheme
+
 
 /**
  * Created by Onikoyi Damola Olutoba
@@ -8,3 +21,34 @@ package com.dev.olutoba.xplorejetcompose.ui.components
  */
 
 
+@Composable
+fun TipTimeLayout() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .statusBarsPadding()
+            .safeDrawingPadding()
+            .padding(horizontal = 40.dp)
+            .safeDrawingPadding()
+    ) {
+        Text(
+            text = "Calculate Tip",
+            modifier = Modifier
+
+        )
+
+        Text(text = "Bill Amount")
+
+        Text(text = "Tip Amount: $15.00")
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PreviewTipTimeLayout() {
+    XploreJetComposeTheme {
+        TipTimeLayout()
+    }
+}
