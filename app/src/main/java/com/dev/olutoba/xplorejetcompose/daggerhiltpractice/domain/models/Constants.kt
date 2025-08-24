8e49d01ac56542d5a82c5239c13d87c99b057427
Damola Olutoba Onikoyi <damexxey94@gmail.com>
2025-08-24 12:43:13 +0100

    create Constants.kt file

---
+++ app/src/main/java/com/dev/olutoba/xplorejetcompose/daggerhiltpractice/domain/models/Constants.kt
@@ -0,0 +1,15 @@
+package com.dev.olutoba.xplorejetcompose.daggerhiltpractice.domain.models
+
+
+/**
+ * Created by Onikoyi Damola Olutoba
+ * DATE: 22 August, 2025
+ * EMAIL: damexxey94@gmail.com
+ */
+
+object Constants {
+    const val BASE_URL = "https://github.com/Olutobz"
+    const val X_URL = "https://x.com/IkoyiOlutoba"
+    const val CONNECT_TIMEOUT = 15L
+    const val READ_TIMEOUT = 15L
+}
