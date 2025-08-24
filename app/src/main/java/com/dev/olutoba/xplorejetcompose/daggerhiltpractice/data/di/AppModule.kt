6c080172ef92482c9b91298d17ba889cdd1783e5
Damola Olutoba Onikoyi <damexxey94@gmail.com>
2025-08-24 12:43:52 +0100

    update DI app module

--- app/src/main/java/com/dev/olutoba/xplorejetcompose/daggerhiltpractice/data/di/AppModule.kt
+++ app/src/main/java/com/dev/olutoba/xplorejetcompose/daggerhiltpractice/data/di/AppModule.kt
@@ -2,13 +2,22 @@ package com.dev.olutoba.xplorejetcompose.daggerhiltpractice.data.di
 
 import com.dev.olutoba.xplorejetcompose.daggerhiltpractice.api.SampleApiService
 import com.dev.olutoba.xplorejetcompose.daggerhiltpractice.data.datasource.remote.SampleRemoteDataSource
-import com.dev.olutoba.xplorejetcompose.daggerhiltpractice.data.repository.SampleRepositoryImpl
-import com.dev.olutoba.xplorejetcompose.daggerhiltpractice.domain.repository.SampleRepository
+import com.dev.olutoba.xplorejetcompose.daggerhiltpractice.domain.models.Constants.BASE_URL
+import com.dev.olutoba.xplorejetcompose.daggerhiltpractice.domain.models.Constants.CONNECT_TIMEOUT
+import com.dev.olutoba.xplorejetcompose.daggerhiltpractice.domain.models.Constants.READ_TIMEOUT
+import com.dev.olutoba.xplorejetcompose.daggerhiltpractice.domain.models.Constants.X_URL
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
 import javax.inject.Singleton
 
 
@@ -24,12 +33,77 @@ import javax.inject.Singleton
 object AppModule {
 
     @Provides
+    @Named("baseUrl")
+    fun provideBaseUrl(): String = BASE_URL
+
+    @Provides
+    @Named("XbaseUrl")
+    fun provideXBaseUrl(): String = X_URL
+
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    @Provides
     @Singleton
-    fun provideSampleApiService(): SampleApiService {
-        return Retrofit.Builder()
-            .baseUrl("https://github.com/Olutobz")
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
             .build()
-            .create(SampleApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideConverterFactory(moshi: Moshi): MoshiConverterFactory {
+        return MoshiConverterFactory.create(moshi)
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(timeout = CONNECT_TIMEOUT, unit = TimeUnit.SECONDS)
+            .readTimeout(timeout = READ_TIMEOUT, unit = TimeUnit.SECONDS)
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    @BaseRetrofit
+    fun provideBaseRetrofit(
+        @Named("baseUrl") baseUrl: String,
+        okHttpClient: OkHttpClient,
+        moshiConverterFactory: MoshiConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(okHttpClient)
+            .addConverterFactory(moshiConverterFactory)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideXRetrofit(
+        @Named("XbaseUrl") baseUrl: String,
+        okHttpClient: OkHttpClient,
+        moshiConverterFactory: MoshiConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(okHttpClient)
+            .addConverterFactory(moshiConverterFactory)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideSampleApiService(retrofit: Retrofit): SampleApiService {
+        return retrofit.create(SampleApiService::class.java)
     }
 
     @Provides
@@ -38,14 +112,6 @@ object AppModule {
         sampleApiService: SampleApiService
     ): SampleRemoteDataSource {
         return SampleRemoteDataSource(sampleApiService)
-    }
-
-    @Provides
-    @Singleton
-    fun provideSampleRepository(
-        sampleRemoteDataSource: SampleRemoteDataSource
-    ): SampleRepository {
-        return SampleRepositoryImpl(sampleRemoteDataSource)
     }
 
 }
