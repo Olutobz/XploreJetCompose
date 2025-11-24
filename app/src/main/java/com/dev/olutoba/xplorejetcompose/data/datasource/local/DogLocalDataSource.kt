7ebc9e31bd0b0ff04cf87b05e79a5f6595388712
Damola Olutoba Onikoyi <damexxey94@gmail.com>
2025-11-24 19:38:49 +0100

    modify DogLocalDataSource.kt

--- app/src/main/java/com/dev/olutoba/xplorejetcompose/data/datasource/local/DogLocalDataSource.kt
+++ app/src/main/java/com/dev/olutoba/xplorejetcompose/data/datasource/local/DogLocalDataSource.kt
@@ -25,10 +25,22 @@ fun loadDogsInfo(): List<Dog> {
             imageResourceId = R.drawable.lola
         ),
         Dog(
+            name = R.string.dog_name_2,
+            age = 9,
+            hobbies = R.string.dog_description_2,
+            imageResourceId = R.drawable.lola
+        ),
+        Dog(
             name = R.string.dog_name_3,
             age = 11,
             hobbies = R.string.dog_description_3,
             imageResourceId = R.drawable.frankie
+        ),
+        Dog(
+            name = R.string.dog_name_4,
+            age = 9,
+            hobbies = R.string.dog_description_4,
+            imageResourceId = R.drawable.nox
         ),
         Dog(
             name = R.string.dog_name_4,
@@ -49,6 +61,12 @@ fun loadDogsInfo(): List<Dog> {
             imageResourceId = R.drawable.bella
         ),
         Dog(
+            name = R.string.dog_name_6,
+            age = 8,
+            hobbies = R.string.dog_description_6,
+            imageResourceId = R.drawable.bella
+        ),
+        Dog(
             name = R.string.dog_name_7,
             age = 3,
             hobbies = R.string.dog_description_7,
@@ -59,6 +77,18 @@ fun loadDogsInfo(): List<Dog> {
             age = 2,
             hobbies = R.string.dog_description_8,
             imageResourceId = R.drawable.tzeitel
+        ),
+        Dog(
+            name = R.string.dog_name_8,
+            age = 2,
+            hobbies = R.string.dog_description_8,
+            imageResourceId = R.drawable.tzeitel
+        ),
+        Dog(
+            name = R.string.dog_name_9,
+            age = 5,
+            hobbies = R.string.dog_description_9,
+            imageResourceId = R.drawable.leroy
         ),
         Dog(
             name = R.string.dog_name_9,
