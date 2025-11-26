ccd382cca98556d7f33c6223ddb5e21205fdee58
Damola Olutoba Onikoyi <damexxey94@gmail.com>
2025-11-26 23:08:58 +0100

    modify DogApp.kt

--- app/src/main/java/com/dev/olutoba/xplorejetcompose/ui/view/screens/DogApp.kt
+++ app/src/main/java/com/dev/olutoba/xplorejetcompose/ui/view/screens/DogApp.kt
@@ -9,22 +9,32 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dev.olutoba.xplorejetcompose.R
 import com.dev.olutoba.xplorejetcompose.data.datasource.local.loadDogsInfo
 import com.dev.olutoba.xplorejetcompose.domain.models.Dog
+import com.dev.olutoba.xplorejetcompose.ui.theme.Shapes
 import com.dev.olutoba.xplorejetcompose.ui.theme.WoofTheme
 
 
@@ -35,22 +45,63 @@ import com.dev.olutoba.xplorejetcompose.ui.theme.WoofTheme
  */
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DogScreen() {
-    Surface(
-        color = MaterialTheme.colorScheme.background,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        DogList()
+    Scaffold(
+        topBar = { DogTopAppBar() },
+        modifier = Modifier.padding(8.dp)
+    ) { innerPadding ->
+        Surface(
+            color = MaterialTheme.colorScheme.background,
+            modifier = Modifier
+                .fillMaxSize()
+                .statusBarsPadding()
+                .padding(innerPadding)
+        ) {
+            DogList()
+        }
     }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DogTopAppBar() {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_woof_logo),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(dimensionResource(R.dimen.image_size))
+                        .padding(dimensionResource(R.dimen.padding_small))
+                )
+
+                Text(
+                    text = stringResource(R.string.woof),
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        }
+    )
 }
 
 
 @Composable
 fun DogList() {
-    LazyColumn {
-        items(items = loadDogsInfo()) { dog ->
-            DogItem(dog = dog)
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+        modifier = Modifier
+            .padding(dimensionResource(R.dimen.padding_small))
+    ) {
+        items(items = loadDogsInfo()) {
+            DogItem(dog = it)
         }
     }
 }
@@ -58,37 +109,43 @@ fun DogList() {
 
 @Composable
 fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
-    Row(
-        horizontalArrangement = Arrangement.Start,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(dimensionResource(R.dimen.padding_small))
-    ) {
-        Image(
-            painter = painterResource(dog.imageResourceId),
-            contentDescription = null,
+    Card(modifier = modifier) {
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
             modifier = modifier
-                .size(dimensionResource(R.dimen.image_size))
+                .fillMaxWidth()
                 .padding(dimensionResource(R.dimen.padding_small))
-        )
-
-        Column {
-            Text(
-                text = stringResource(dog.name),
-                style = MaterialTheme.typography.bodyMedium,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier
-                    .padding(top = dimensionResource(R.dimen.padding_small))
+        ) {
+            Image(
+                painter = painterResource(dog.imageResourceId),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = modifier
+                    .size(dimensionResource(R.dimen.image_size))
+                    .padding(dimensionResource(R.dimen.padding_small))
+                    .clip(shape = Shapes.small)
             )
 
-            Text(
-                text = stringResource(R.string.years_old, dog.age),
-                style = MaterialTheme.typography.bodySmall
-            )
+            Column {
+                Text(
+                    text = stringResource(dog.name),
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.Default,
+                    modifier = Modifier
+                        .padding(top = dimensionResource(R.dimen.padding_small))
+                )
+
+                Text(
+                    text = stringResource(R.string.years_old, dog.age),
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Preview(
