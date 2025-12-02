8642c7b9af68a254f6685b0e4d54a4f2c2165809
Damola Olutoba Onikoyi <damexxey94@gmail.com>
2025-12-02 23:10:13 +0100

    add Shape.kt and woof icon

---
+++ app/src/main/java/com/dev/olutoba/xplorejetcompose/ui/theme/Shape.kt
@@ -0,0 +1,17 @@
+package com.dev.olutoba.xplorejetcompose.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+
+/**
+ * Created by Onikoyi Damola Olutoba
+ * DATE: 26 November, 2025
+ * EMAIL: damexxey94@gmail.com
+ */
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(50.dp),
+    medium = RoundedCornerShape(bottomStart = 16.dp, topEnd = 16.dp)
+)
