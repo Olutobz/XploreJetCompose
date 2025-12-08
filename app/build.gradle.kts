a99a27bc0c23ff91fb409f6ac2e403ea63c4daf5
Damola Olutoba Onikoyi <damexxey94@gmail.com>
2025-12-08 10:44:57 +0100

    modify build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.material.icons.extended)
 
     // Compose BOM
     implementation(platform(libs.androidx.compose.bom))
